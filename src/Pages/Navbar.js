aec8da479c3bcc26e73a4ea6cb58b869aa0ac6be
Daniel Owusu <danielowusu326@hotmail.co.uk>
2023-08-26 22:00:21 +0000

    Made changes to the Navbar Component

--- src/Pages/Navbar.js
+++ src/Pages/Navbar.js
@@ -1,34 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-      <div> 
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-    <Link to='/Home'> <a class="navbar-brand" href="#">CAPELLA</a></Link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Services</a>
-          </li>
-        </ul>
-      </div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container">
+          <Link to="/Home" className="navbar-brand">
+            CAPELLA
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <Link to="/Home" className="nav-link active">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/FAQ" className="nav-link">
+                  About us
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  </nav>
-      </div>
-  )
+  );
 }
 
-export default Navbar
-
-
+export default Navbar;
